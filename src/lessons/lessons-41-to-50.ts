1db551dc5ad53b6b8dfdf6919ac891c10b2950c9
hoann-msi <hoann-msi@kimngoctek.com>
2025-09-04 11:19:04 +0700

    update

--- src/lessons/lessons-41-to-50.ts
+++ src/lessons/lessons-41-to-50.ts
@@ -9,15 +9,12 @@ export function nextPermutation(nums: number[]): void {
 
     if (i >= 0) {
         let j = n - 1;
-        // 2. Find element just larger than nums[i]
         while (nums[j] <= nums[i]) {
             j--;
         }
-        // 3. Swap nums[i] and nums[j]
         [nums[i], nums[j]] = [nums[j], nums[i]];
     }
 
-    // 4. Reverse suffix (i+1 to end)
     let left = i + 1;
     let right = n - 1;
     while (left < right) {
